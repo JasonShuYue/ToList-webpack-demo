70b25eb63712e44c58521387e6d96862d46d089d
Jason_Shu <shuyue524@gmail.com>
2019-02-03 22:44:21 +0800

    引入Jquery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import j from 'jquery'
 
 function component() {
-    let element = document.createElement('div');
+    let element = j('<div></div>');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
+    element.html(_.join(['Hello','webpack'], ' '))
+   
+    return element.get(0);
 }
 
 document.body.appendChild(component());
