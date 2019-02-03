a0de67dc0edfaf528fb67cdd509c99d2d1a921a1
Jason_Shu <shuyue524@gmail.com>
2019-02-03 23:00:08 +0800

    add foo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
-import j from 'jquery'
+import j from 'jquery';
+import foo from './foo.js';
 
 function component() {
     let element = j('<div></div>');
 
     element.html(_.join(['Hello','webpack'], ' '))
-   
+
     return element.get(0);
 }
 
 document.body.appendChild(component());
+
+console.log(foo);
+console.log(foo())
