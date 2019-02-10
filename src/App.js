f2df8f36e9d50d082c6ef4a3576cf3f858a89e53
Jason_Shu <shuyue524@gmail.com>
2019-02-10 21:44:57 +0800

    add normalize.css reset.css App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import 'normalize.css';
 
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 
 import './App.css';
+import './reset.css';
 
 class App extends Component {
   constructor(props) {
