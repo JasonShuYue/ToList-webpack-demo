5672d20217b9b89f17eef9c558b54e2e1a0a63a3
Jason_Shu <shuyue524@gmail.com>
2019-02-10 15:11:33 +0800

    使用webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import j from 'jquery';
 import foo from './foo.js';
+import printMe from './print';
 
 function component() {
     let element = j('<div></div>');
 
-    element.html(_.join(['Hello','webpack'], ' '))
+    element.html(_.join(['Hello','webpack', '12312355555'], ' '))
 
     return element.get(0);
 }
@@ -14,3 +15,6 @@ document.body.appendChild(component());
 
 console.log(foo);
 console.log(foo())
+
+
+printMe();
